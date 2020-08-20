45d30826cd12ec0bd26c16eaf26010f075d8e8cb
yaolh <347046045@qq.com>
2020-08-20 11:14:40 +0800

    JS部分，数据初始化完成=>准备修改成TS

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,13 +17,15 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import {Watch} from "vue-property-decorator";
-
-const x = localStorage.getItem('x')
-const dataInit = JSON.parse(x);
 export default {
   name: "Money",
   components: {Tags, Notes, Types, NumberPad},
   data() {
+    let recordList=[];
+    const x = localStorage.getItem('x');
+    if (x !== undefined) {
+    recordList = JSON.parse(x) ||[];
+    }
     return {
       dataSource: {
         tags: ['吃饭', '请客', '买', '交通'],
@@ -36,44 +38,58 @@ export default {
         type: '-',
         number: "0",
         notes: '',
-        createAt:''
+        createAt: ''
       },
-      recordList: []
-    }
+      recordList
+  }
   },
   methods: {
+    dataInit() {
+      recordList=[];
+      const x = localStorage.getItem('x');
+      let recordList = JSON.parse(x);
+      return recordList
+    },
     onUpdateDataSource(newTags) {
       this.dataSource.tags = newTags
-    },
+    }
+    ,
     onUpdateSelectedTags(newSelectedTags) {
       this.dataSource.selectedTags = newSelectedTags;
       this.record.tags = this.dataSource.selectedTags
-    },
+    }
+    ,
     onUpdateType(type) {
       this.record.type = type;
-    },
+    }
+    ,
     onUpdateNotes(value) {
       this.record.notes = value;
-    },
+    }
+    ,
     onUpdateNumber(number) {
       this.record.number = number
-    },
+    }
+    ,
     onUpdateRecord() {
       let record1 = JSON.parse(JSON.stringify(this.record))
       record1.createAt = new Date();
       this.recordList.push(record1)
     }
-  },
-  watch: {
-    'recordList': {
-      handler() {
-        window.alert('记了一笔'+"“"+this.record.notes+"”")
-        let save=JSON.stringify(this.recordList)
-        localStorage.setItem("x", save)
-      }
-    }
   }
-};
+  ,
+  watch: {
+    'recordList':
+        {
+          handler() {
+            window.alert('记了一笔' + "“" + this.record.notes + "”")
+            let save = JSON.stringify(this.recordList)
+              localStorage.setItem("x", save)
+          }
+        }
+  }
+}
+;
 </script>
 
 <style lang="scss">
